a27590dedf8a41e353b7fe92c87d00285a76ebd4
Jam Creencia <jrcreencia@gmail.com>
2025-01-22 16:34:49 +0000

    Convert to TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const API_KEY = "<INSERT_HERE>";
+
+function App() {
+	return (
+		<div className="App">
+			<input />
+			<button>Add City</button>
+		</div>
+	);
+}
+
+export default App;
